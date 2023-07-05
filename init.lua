21e43beb3cded7d81418a9691252699421ae3334
Robdres <robdres123@gmail.com>
2023-07-04 19:18:47 -0500

    Added

--- init.lua
+++ init.lua
@@ -4,6 +4,7 @@ vim.opt.updatetime = 300
 vim.opt.signcolumn = "yes"
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.g.textwidth = 80
 
 vim.opt.backspace = '2'
 vim.opt.showcmd = true
@@ -11,8 +12,9 @@ vim.opt.laststatus = 2
 vim.opt.autowrite = true
 vim.opt.autoread = true
 
-vim.cmd [[colorscheme gruvbox]]
+vim.cmd [[colorscheme habamax]]
 -- use spaces for tabs and whatnot  
+--
 vim.wo.signcolumn = "yes"
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
@@ -41,7 +43,20 @@ vim.cmd [[packadd packer.nvim]]
 require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'rstacruz/vim-closer'
+  use 'tpope/vim-fugitive'
+  use({
+    "iamcco/markdown-preview.nvim",
+    run = function() vim.fn["mkdp#util#install"]() end,
+  })
+
   use 'lervag/vimtex'
+  use 'GCBallesteros/jupytext.vim'
+
+  use { "kiyoon/jupynium.nvim", run = "pip3 install --user ." }
+
+  use { 'dccsillag/magma-nvim', run = ':UpdateRemotePlugins' }
+  use 'honza/vim-snippets'
+
   use{ 'anuvyklack/pretty-fold.nvim',
     config = function()
         require('pretty-fold').setup()
@@ -49,8 +64,7 @@ require('packer').startup(function(use)
   }
   use {'tpope/vim-dispatch', opt = true, cmd = {'Dispatch', 'Make', 'Focus', 'Start'}}
   use {'andymass/vim-matchup', event = 'VimEnter'}
-  use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', cmd = 'MarkdownPreview'}
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
   use "rafamadriz/friendly-snippets"
   use 'neoclide/coc.nvim'
   use 'altercation/vim-colors-solarized'
@@ -61,6 +75,7 @@ require('packer').startup(function(use)
   use 'scrooloose/nerdcommenter'
   use 'christoomey/vim-tmux-navigator'
   use 'sheerun/vim-polyglot'
+
   use {'romgrk/barbar.nvim', requires = {
     'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
     'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
@@ -78,6 +93,7 @@ require('packer').startup(function(use)
   use 'hail2u/vim-css3-syntax'
   use 'preservim/nerdtree'
   use 'ckunte/latex-snippets-vim'
+
 end)
 
 
@@ -104,12 +120,30 @@ keyset("n", "gy", "<Plug>(coc-type-definition)", {silent = true})
 keyset("n", "gi", "<Plug>(coc-implementation)", {silent = true})
 keyset("n", "gr", "<Plug>(coc-references)", {silent = true})
 
-
--- Move to previous/next
+-- Buffer/Tabs control
 vim.cmd [[nnoremap <silent>    H <Cmd>BufferPrevious<CR>]]
 vim.cmd [[nnoremap <silent>    L <Cmd>BufferNext<CR>]]
+vim.cmd [[nnoremap <silent>    <leader>d <Cmd>BufferClose<CR>]]
+
+-- Nerd tree
 vim.cmd [[nnoremap <silent>    <leader>f  <Cmd>NERDTreeToggle<CR>]]
 vim.cmd [[nnoremap <silent>    <leader>t <Cmd>terminal<CR>]]
+
 vim.cmd [[tnoremap <Esc> <C-\><C-n>]]
+vim.cmd [[let g:airline#extensions#tabline#formatter = '']]
 vim.cmd [[set scrolloff=10]]
 
+vim.cmd [[let g:jupytext_fmt = 'py']]
+vim.cmd [[let g:jupytext_style = 'hydrogen']]
+
+vim.cmd [[
+nnoremap <silent><expr> <LocalLeader>r  :MagmaEvaluateOperator<CR>
+nnoremap <silent>       <LocalLeader>rr :MagmaEvaluateLine<CR>
+xnoremap <silent>       <LocalLeader>r  :<C-u>MagmaEvaluateVisual<CR>
+nnoremap <silent>       <LocalLeader>rc :MagmaReevaluateCell<CR>
+nnoremap <silent>       <LocalLeader>rd :MagmaDelete<CR>
+nnoremap <silent>       <LocalLeader>ro :MagmaShowOutput<CR>
+
+let g:magma_automatically_open_output = v:false
+let g:magma_image_provider = "ueberzug"
+]]
